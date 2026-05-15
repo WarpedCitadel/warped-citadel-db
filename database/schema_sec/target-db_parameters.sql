e28260d27df9e232e21f7ca163af0868df742700
Jaeger Nolte <jaeger.nolte@gmail.com>
2026-05-15 11:57:17 -0500

    updated schema sec

--- database/schema_sec/target-db_parameters.sql
+++ database/schema_sec/target-db_parameters.sql
@@ -8,34 +8,34 @@
 *    Edit the parameter values below for the target database.
 *
 *    Rename the file replacing "target-db" with the target database name.
-*        e.g. pega_dev_parameters.sql
+*        e.g. wc_dev_parameters.sql
 *
 *    When executing created_database.sql or rip_database.sql with the psql client, pass
 *    in a variable named "vdbname" with the value of your target database name used when
 *    renaming this file. The created_database.sql and rip_database.sql scripts will then dynamically
 *    execute the correct parameters file.
 *
-*    Ex: psql --host=localhost --username=postgres --dbname=postgres --echo-all -f create_database.sql -v vdbname='pega_dev'
+*    Ex: psql --host=localhost --username=postgres --dbname=postgres --echo-all -f create_database.sql -v vdbname='wc_dev'
 *
 *
 *
 ***************************************************************************************/
 
--- Phoenix Database
+-- Warped Citadel Database
 \set dbname                    'wc_dev'
 
 
 -- Schema Owner User
 \set schema_owner              'dbo_wc'
-\set schema_owner_pwd          'changeme'
+\set schema_owner_pwd          'changme'
 
 
 -- Secure Users
 \set app_reader_user           'wc_app_reader'
-\set app_reader_pwd            'changeme'
+\set app_reader_pwd            'changme'
 
 \set app_sec_user              'wc_app_secure'
-\set app_sec_pwd               'changeme'
+\set app_sec_pwd               'changme'
 
 
 -- Secure Roles (DO NOT CHANGE!)
