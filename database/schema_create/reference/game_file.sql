49f2fd5e21acd785920b0efc48654eff881cc96e
Jaeger Nolte <jaeger.nolte@gmail.com>
2026-07-20 21:41:34 -0500

    Created staging table for game files for backend processing

--- database/schema_create/reference/game_file.sql
+++ database/schema_create/reference/game_file.sql
@@ -1,4 +1,4 @@
-insert into game_file (game_profile_id, file_uuid, file_name, file_version, file_size, platform_id, status_type_id) values (1 ,'019f58df-9cee-7a48-9ffd-37f57e23fde8', 'IceCave1.1.zip', 'v1.6.9', '34MB', 2, 4);
-insert into game_file (game_profile_id, file_uuid, file_name, file_version, file_size, platform_id, status_type_id) values (2 ,'019f58ee-fd8e-778c-9d31-f3c9a2febf82', 'index.audio.worklet.zip', '1.0.0', '10MB', 1, 4);
-insert into game_file (game_profile_id, file_uuid, file_name, file_version, file_size, platform_id, status_type_id) values (3 ,'019f5900-6315-7879-99f7-6f64037ba886', 'mountainMechanicV1.2.zip', 'v1.0.1', '77MB', 2, 4);
-insert into game_file (game_profile_id, file_uuid, file_name, file_version, file_size, platform_id, status_type_id) values (4 ,'019f5e89-dc89-7809-8994-5b53415d1915', 'Planet Game Tech Demo0.1 - Web.zip', 'v1.6.7', '18MB', 1, 4);
+insert into game_file (game_profile_id, file_uuid, file_name, file_size, platform_id, status_type_id) values (1 ,'019f58df-9cee-7a48-9ffd-37f57e23fde8', 'IceCave1.1.zip', '34MB', 2, 4);
+insert into game_file (game_profile_id, file_uuid, file_name, file_size, platform_id, status_type_id) values (2 ,'019f58ee-fd8e-778c-9d31-f3c9a2febf82', 'index.audio.worklet.zip', '10MB', 1, 4);
+insert into game_file (game_profile_id, file_uuid, file_name, file_size, platform_id, status_type_id) values (3 ,'019f5900-6315-7879-99f7-6f64037ba886', 'mountainMechanicV1.2.zip', '77MB', 2, 4);
+insert into game_file (game_profile_id, file_uuid, file_name, file_size, platform_id, status_type_id) values (4 ,'019f5e89-dc89-7809-8994-5b53415d1915', 'Planet Game Tech Demo0.1 - Web.zip', '18MB', 1, 4);
--- database/schema_create/tables/game_file.sql
+++ database/schema_create/tables/game_file.sql
@@ -1,12 +1,11 @@
 --liquibase formatted sql
 
---changeset jnolte:20260615_create_game_file
+--changeset jnolte:20260720_create_game_file
 CREATE TABLE game_file (
   id 					SERIAL 			NOT NULL PRIMARY KEY,
   game_profile_id		BIGINT			NOT NULL REFERENCES game_profile (id),
   file_uuid				UUID			NOT NULL DEFAULT uuidv7(),
   file_name 			VARCHAR(100)	NOT NULL,
-  file_version 			VARCHAR(20)		NOT NULL,
   file_size 			VARCHAR(7)		NOT NULL,
   platform_id			SMALLINT		NOT NULL REFERENCES platform (id),
   status_type_id 		SMALLINT 		NOT NULL REFERENCES status_type (id) DEFAULT 1,
